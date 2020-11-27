45333905c6984ec341820b67ac42d88f0d3d6d59
Terence <1403973494@qq.com>
2020-11-27 14:15:38 +0800

    测试提交代码

---
+++ Project2/Project2/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a;
+	int b;
+	int num1 = 0;
+	int num2 = 0;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+	int sum;
+	printf_s("输入两个操作数:>");
+	scanf_s("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum = %d\n", sum);
+	system("pause");
+	return 0;
+}
