e9a63c7c1d3213cd9c1cf2664f9f2cc7c8fc67bd
Terence <1403973494@qq.com>
2021-03-05 14:33:12 +0800

    链表删除、逆置

--- List/src/Study0304/ListApply2.java
+++ List/src/Study0304/ListApply2.java
@@ -1,18 +1,20 @@
 package Study0304;
+
+
 class ListIsNullException extends RuntimeException{
     public ListIsNullException(String message){
         super(message);
 
     }
 }
-class Node{
+class Node{//定义节点
     int data;
     Node next;
     public Node(int data){
         this.data = data;
         this.next = null;
     }
-    @Override
+    @Override//重写toString方法
     public String toString(){
         return "[" + data + "]";
     }
@@ -37,10 +39,13 @@ public class ListApply2 {
 
         Node head = n1;
 
-        head = pushFront(head, 10);
-        head = popFront(head, 12);
-        head = pushBack(head);
-        head = popBack(head);
+//        head = pushFront(head, 10);
+//        head = popFront(head, 12);
+//        head = pushBack(head);
+//        head = popBack(head);
+
+//        head = reverse(head);
+        head = remove(head,1);
         while (head != null){
             System.out.println(head);
             head = head.next;
@@ -48,6 +53,50 @@ public class ListApply2 {
 
 
 }
+
+    private static Node remove(Node head, int i) {//给定值删除元素
+        Node prev = head;
+        Node cur = head.next;
+        if (head.next == null){//如果是空链表
+            return head;
+        }
+        for (; cur != null; ){
+            if (cur.data == i){//如果值相同
+                prev.next = cur.next;
+                cur = prev.next;
+
+            }else{//否则更新指针
+                prev = prev.next;
+                cur = cur.next;
+            }
+        }
+        if (head.data == i){//如果给定的值是头结点
+            head = head.next;
+        }
+        return head;
+    }
+
+    private static Node reverse(Node head) {//链表逆置
+        if(head == null){
+            return null;
+        }
+        if (head.next == null){
+            return head;
+        }
+        Node curNode = head;
+        Node prevNode = null;
+        Node newHead = null;
+        while (curNode != null){
+            Node nextNode = curNode.next;
+            if (nextNode == null){
+                newHead = curNode;
+            }
+            curNode.next = prevNode;//掰道岔
+            prevNode = curNode;//更新指针
+            curNode = nextNode;
+        }
+        return newHead;
+    }
 
     private static Node popBack(Node head) {//尾删
         if (head == null){
