02a30bd5cfc9c1e6fe42001cfa5cf3e655d1e61e
Terence <1403973494@qq.com>
2021-03-12 23:26:30 +0800

    更新

--- List/src/study0308/MyStack.java
+++ List/src/study0308/MyStack.java
@@ -1,46 +1,68 @@
 package study0308;
 
+
+
 public class MyStack {
-  private int[] data = new int[100];
-  private int size = 0;
+    private int capacity = 2;
+  private int[] data = new int[capacity];
+  int size = 0;
 
-  //入栈
-
-    public void push(int val){
-        if(size >= data.length){
-            //进行扩容操作
-
-            size = 2 * size;
-            int newData[] = new int[size];
-            for (int i = 0; i < size; i++) {
-                newData[i] = data[i];
-                data = newData;
-                return;
-            }
-
-            data[size] = val;
-            size++;
+  public void push(int val){
+      if (size >= data.length){
+          realloc();
+      }
+      data[size] = val;
+      size++;
+  }
+    private void realloc() {
+        capacity = 2 * capacity;
+        int[] newData = new int[capacity];
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
         }
+        data = newData;
+    }
+  public int pop(){
+      if (size == 0){
+          return -1;
+      }
+      int index = size - 1;
+      size--;
+      return data[index];
+
+  }
+
+  public int peek(){
+      if (size == 0){
+          return -1;
+      }
+      return data[size - 1];
+  }
+
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(5);
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(5);
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(8);
+
+
+
+        System.out.println(myStack.pop());
+        System.out.println(myStack.peek());
+
     }
 
-    //出栈
-    public Integer pop(){
-        if(size == 0){
-            return  null;
-        }
 
-        int ret = data[size - 1];
-        size--;
-        return ret;
-    }
-
-    //取栈顶元素
-
-    public Integer peek(){
-        if (size == 0){
-            return  null;
-        }
-
-        return data[size - 1];
-    }
 }
