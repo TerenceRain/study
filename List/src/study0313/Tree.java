0623c938c4e36388294c1724c8e6b27595d055ae
Terence <1403973494@qq.com>
2021-03-13 23:36:15 +0800

    更新代码

---
+++ List/src/study0313/Tree.java
@@ -0,0 +1,25 @@
+package study0313;
+class TreeNode{
+    String val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(String val){
+        this.val = val;
+    }
+}
+
+public class Tree {
+    public static void creatTree(){
+        TreeNode a = new TreeNode("A");
+        TreeNode b = new TreeNode("B");
+        TreeNode c = new TreeNode("C");
+        TreeNode d = new TreeNode("D");
+        TreeNode e = new TreeNode("E");
+        TreeNode f = new TreeNode("F");
+        TreeNode g = new TreeNode("G");
+
+        a.left = 
+
+    }
+}
