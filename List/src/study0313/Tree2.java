d1c3c5e5c1f0a84ab1c12b1cca652102e63c2020
Terence <1403973494@qq.com>
2021-03-16 21:38:15 +0800

    更新代码

--- List/src/study0313/Tree2.java
+++ List/src/study0313/Tree2.java
@@ -18,6 +18,9 @@ public class Tree2 {
         Node f = new Node("F");
         Node g = new Node("G");
 
+
+
+
         a.left = b;
         a.right = c;
         b.left = d;
@@ -64,12 +67,63 @@ public class Tree2 {
         postOrder(root.right);
         // 最后打印根节点
         System.out.print(root.val);
+
     }
 
     public static void main(String[] args) {
         Node root = build();
 //        preOrder(root);
 //        inOrder(root);
-        postOrder(root);
+//        postOrder(root);
+//        System.out.println(calculateNode(root));
+//        System.out.println(calculateLevelNode(root));
+//        System.out.println(calcuTheKth(root, 3));
+        System.out.println(find(root, "F"));
     }
+
+    private static Node find(Node root, String toFind) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val.equals(toFind)) {
+            return root;
+        }
+        Node result = find(root.left, toFind);
+        if (result != null) {
+            return result;
+        }
+        return find(root.right, toFind);
+    }
+
+    private static int calcuTheKth(Node root,int k) {
+        if (root == null || k < 1){
+            return 0;
+        }
+        if (k == 1) {
+            return 1;
+        }
+        return calcuTheKth(root.left, k - 1) + calcuTheKth(root.right, k - 1);
+    }
+
+    private static int calculateLevelNode(Node root) {//统计叶子节点的个数
+        if (root == null){
+            return 0;
+        }
+        if (root.left == null && root.right == null){
+            return 1;
+        }
+        return calculateLevelNode(root.left) + calculateLevelNode(root.right);
+    }
+
+    private static int calculateNode(Node root) {//统计节点个数
+        if(root == null) {
+            return 0;
+        }
+        if (root.right == null && root.left == null){
+            return 1;
+        }
+        return 1+ calculateNode(root.left) + calculateNode(root.right);
+    }
+
+
 }
---
+++ List/src/study0316/solution.java
@@ -0,0 +1,132 @@
+package study0316;
+
+import javax.swing.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class solution {
+    public static TreeNode creatTree() {
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(4);
+        TreeNode e = new TreeNode(5);
+        TreeNode f = new TreeNode(6);
+        TreeNode g = new TreeNode(7);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+//        c.left = e;
+//        e.left = g;
+//        e.right = f;
+
+        return a;
+
+    }
+    public static boolean isCompleteTree(TreeNode root){//判断完全二叉树
+        if (root == null){
+            return  true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean isLevel2 = false;
+        queue.offer(root);
+        while (true) {
+            TreeNode cur = queue.poll();
+            if (cur == null) {
+                break;
+            }
+            if (isLevel2) {
+                if (root.left != null && root.right != null) {
+                    queue.offer(root.left);
+                    queue.offer(root.right);
+                } else if (root.left == null && root.right != null) {
+                    return false;
+                } else if (root.left != null && root.right == null) {
+                    isLevel2 = true;
+                    queue.offer(root.left);
+                }else{
+                    isLevel2 = true;
+                }
+            }else {
+                if (root.left != null && root.right != null){
+                    return false;
+                }
+            }
+        }
+return true;
+
+    }
+    public static void levelOrderTraversal(TreeNode root) {//实现层序遍历
+        if (root == null) {
+            return;
+        }
+        Queue queue = new LinkedList();
+        queue.offer(root);
+        while (true) {
+            TreeNode cur = (TreeNode) queue.poll();
+            if (cur == null) {
+                break;
+            }
+
+            System.out.println(cur.val);
+            if (cur.left != null) {
+                queue.offer(cur.left);
+
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+        }
+    }
+        public static boolean isSameTree (TreeNode p, TreeNode q){
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            if (p == q && p.left == null && p.right == null && q.left == null && q.right == null) {
+                return true;
+            }
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        public static int maxDepth (TreeNode root){
+            if (root == null) {
+                return 0;
+            }
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        }
+        public static boolean isBalanced (TreeNode p){
+            if (p == null) {
+                return true;
+            }
+            if (maxDepth(p.left) - maxDepth(p.right) > 1 || maxDepth(p.left) - maxDepth(p.right) < -1) {
+                return false;
+            }
+//        return true;
+            return isBalanced(p.left) && isBalanced(p.right);
+        }
+        public static void main (String[]args){
+            TreeNode p = creatTree();
+            TreeNode q = creatTree();
+//        System.out.println(isSameTree(p, q));
+//        System.out.println(maxDepth(p));
+//            System.out.println(isBalanced(p));
+//            levelOrderTraversal(p);
+            System.out.println(isCompleteTree(p));
+        }
+
+    }
