36b744f96033201a77371a448201d7201e927f28
Terence <1403973494@qq.com>
2021-03-21 19:59:40 +0800

    更新代码

--- List/src/study0318/solution.java
+++ List/src/study0318/solution.java
@@ -104,4 +104,25 @@ public class solution {
             helper(root.right, level + 1);
         }
     }
+    public int index = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder){
+        index = 0;
+        List<Integer> inorderList = new ArrayList<>();
+        for (int x:inorder) {
+            inorderList.add(x);
+        }
+        return _buildTree(preorder, inorderList);
+    }
+
+    private TreeNode _buildTree(int[] preorder, List<Integer> inorder) {
+        if (!inorder.isEmpty()){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[index]);
+        index++;
+        int pos = inorder.indexOf(root.val);
+        root.left = _buildTree(preorder, inorder.subList(0, pos));
+        root.right = _buildTree(preorder, inorder.subList(pos + 1, inorder.size()));
+        return root;
+    }
 }
---
+++ List/src/study0321/BinaryTree.java
@@ -0,0 +1,7 @@
+package study0321;
+
+public class BinaryTree {
+    public int index= 0;
+
+
+}
