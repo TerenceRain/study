993d6c54e995f81b82f2612d422742d1d72533be
Terence <1403973494@qq.com>
2021-04-01 19:24:23 +0800

    HTML

---
+++ List/src/study0331/Solution.java
@@ -0,0 +1,9 @@
+package study0331;
+
+public class Solution {
+    public static void main(String[] args) {
+        int t = 0;
+        System.out.println(t++);
+        System.out.println(t);
+    }
+}
