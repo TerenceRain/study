6757bc763c860653d2af82cd59999d99838d1565
Terence <1403973494@qq.com>
2021-04-03 22:54:39 +0800

    更新代码

--- List/src/study0331/Solution.java
+++ List/src/study0331/Solution.java
@@ -4,6 +4,6 @@ public class Solution {
     public static void main(String[] args) {
         int t = 0;
         System.out.println(t++);
-        System.out.println(t);
+//        System.out.println(t);
     }
 }
