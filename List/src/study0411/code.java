dd10aede1a9516f3d4729d885adbe4f277c155d9
Terence <1403973494@qq.com>
2021-04-12 10:17:58 +0800

    更新代码

---
+++ List/src/study0411/code.java
@@ -0,0 +1,48 @@
+package study0411;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.*;
+public class code {
+
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = null;
+            String b = null;
+            while(sc.hasNextLine()){
+                a = sc.nextLine();
+                b = sc.nextLine();
+            }
+            if(a == null){
+                System.out.println(b);
+            }
+            if(b == null){
+                System.out.println(a);
+            }
+            Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+
+        for(int i = 0; i < a.length(); i++) {
+                char ch1 = a.charAt(i);
+                queue1.offer(Character.toString(ch1));
+            }
+        for (int i = 0; i < b.length(); i++) {
+            char ch2 = b.charAt(i);
+            queue2.offer(Character.toString(ch2));
+        }
+        while(!queue2.isEmpty()){
+            if(queue1.element().equals(queue2.element())){
+                queue1.poll();
+                continue;
+            }else{
+               String str = queue1.poll();
+                System.out.print(str);
+            }
+        }
+
+            System.out.println();
+        }
+    }
+
+
