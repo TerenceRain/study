42406384565b639f911da2cacf18513c3205ef37
Terence <1403973494@qq.com>
2021-04-14 22:55:21 +0800

    更新代码

--- List/src/study0411/code.java
+++ List/src/study0411/code.java
@@ -7,42 +7,50 @@ public class code {
 
 
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            String a = null;
-            String b = null;
-            while(sc.hasNextLine()){
-                a = sc.nextLine();
-                b = sc.nextLine();
-            }
-            if(a == null){
-                System.out.println(b);
-            }
-            if(b == null){
-                System.out.println(a);
-            }
-            Queue<String> queue1 = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        String a = null;
+        String b = null;
+        while (sc.hasNextLine()) {
+            a = sc.nextLine();
+            b = sc.nextLine();
+        }
+        if (a == null) {
+            System.out.println(b);
+        }
+        if (b == null) {
+            System.out.println(a);
+        }
+        Queue<String> queue1 = new LinkedList<>();
         Queue<String> queue2 = new LinkedList<>();
 
-        for(int i = 0; i < a.length(); i++) {
-                char ch1 = a.charAt(i);
-                queue1.offer(Character.toString(ch1));
-            }
+        for (int i = 0; i < a.length(); i++) {
+            char ch1 = a.charAt(i);
+            queue1.offer(Character.toString(ch1));
+        }
         for (int i = 0; i < b.length(); i++) {
             char ch2 = b.charAt(i);
             queue2.offer(Character.toString(ch2));
         }
-        while(!queue2.isEmpty()){
-            if(queue1.element().equals(queue2.element())){
+        while (!queue2.isEmpty()) {
+            if (queue1.element().equals(queue2.element())) {
                 queue1.poll();
                 continue;
-            }else{
-               String str = queue1.poll();
+            } else {
+                String str = queue1.poll();
                 System.out.print(str);
             }
         }
 
-            System.out.println();
+        System.out.println();
+    }
+
+    public static int MoreThanHalfNum_Solution(int[] array) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < array.length; i++) {
+            queue.offer(array[i]);
         }
     }
+}
+
 
 
