7e7058beb5c2a04a69da80e8e0b7dab04ef83502
Terence <1403973494@qq.com>
2021-04-23 18:34:28 +0800

    更新代码

---
+++ List/src/study0423/Main.java
@@ -0,0 +1,26 @@
+package study0423;
+
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        byte n = 31;
+        int n1 = (int)n;
+        String str = Integer.toBinaryString(n);
+        System.out.println(str);
+        int count = 1;
+        char[] ch = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            ch[i] = str.charAt(i);
+        }
+        int i = 0;
+
+        while (i < ch.length - 1){
+            if (ch[i] == '1' && ch[i + 1] == '1' ) {
+            count++;
+            }
+            i++;
+        }
+        System.out.println(count);
+    }
+}
