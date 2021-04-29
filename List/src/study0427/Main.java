eff9762382c0f4ece7d62af10ad9b25862ee30f9
Terence <1403973494@qq.com>
2021-04-29 23:52:59 +0800

    更新代码

--- List/src/study0427/Main.java
+++ List/src/study0427/Main.java
@@ -1,16 +1,38 @@
 package study0427;
 
 import java.util.*;
-class A{
-    public A(String str){
 
-    }
-}
 public class Main{
+
     public static void main(String[] args){
-        A a = new A("he");
-        A a1 = new A("he");
-        System.out.println(a == a1);
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if(i == calcu(i)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+    public static int calcu(int n){
+        int sum = 0;
+        for (int i = 1; i < n; i++){
+            if (n % i == 0){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+//        int i = 5;
+//        char c = '2';
+//      int r = i * c;
+//     double d = r + (double)1;
+//        System.out.println(i * c);
+//        A a = new A("he");
+//        A a1 = new A("he");
+//        System.out.println(a == a1);
 //        Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
 //        int count = 0;
@@ -28,4 +50,3 @@ public class Main{
 //        }
 //        System.out.println(count);
     }
-}
