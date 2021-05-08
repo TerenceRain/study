acf77ba7995b9d2d238d9cf3a4b95dc1112c7906
Terence <1403973494@qq.com>
2021-05-08 23:20:35 +0800

    更新代码

---
+++ List/src/study0508/Main.java
@@ -0,0 +1,27 @@
+package study0508;
+
+public class Main {
+//    public String str = "6";
+//
+//    public static void main(String[] args) {
+//        Main main = new Main();
+//        main.change(main.str);
+//        System.out.println(main.str);
+//    }
+//    public String change(String str){
+//        return str = "10";
+//    }
+//    public static int x = 100;
+//
+//    public static void main(String[] args) {
+//        Main main = new Main();
+//        main.x++;
+//        Main main1 = new Main();
+//        main1.x++;
+//        main = new Main();
+//        main.x++;
+//        Main.x--;
+//        System.out.println(x);
+//
+//    }
+}
