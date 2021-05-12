2bd57c47a7d9668c4497a6908f95c6da1930b58e
Terence <1403973494@qq.com>
2021-05-12 23:28:29 +0800

    更新代码

--- List/src/study0510/Main.java
+++ List/src/study0510/Main.java
@@ -2,7 +2,46 @@ package study0510;
 //思路不对
     import java.util.*;
     public class Main{
+
+
         public static void main(String[] args){
+
+//参数解析
+            Scanner sc = new Scanner(System.in);
+            String str = sc.nextLine();
+            int count = 0;
+            //下面整个for循环都是处理参数个数
+            for(int i = 0; i < str.length(); i++){
+                if(str.charAt(i) == '"'){//因为双引号内的空格不需要计数
+                    do{
+                        i++;
+                    }while(str.charAt(i) != '"');
+                }
+                if(str.charAt(i) == ' '){
+                    count++;
+                }
+
+
+            }
+            System.out.println(count + 1);//输出第一行参数的个数
+            int flag = 1;
+            for(int i = 0; i < str.length(); i++){
+                if(str.charAt(i) == '"'){
+                    flag ^= 1;//这是当前是否在双引号中的标志位，进入引号，置0，出引号，置1
+                }
+                if(str.charAt(i) != ' ' && str.charAt(i) != '"'){//除过双引号和空格剩下的都输出（双引号之外）
+                    System.out.print(str.charAt(i));
+                }
+                if(str.charAt(i) == ' ' && flag != 0){//遇到空格就换行（双引号之外）
+                    System.out.println();
+                }
+                if(str.charAt(i) == ' ' && flag == 0){//双引号之内的空格需要输出
+                    System.out.print(str.charAt(i));
+                }
+            }
+
+
+
             Scanner sc = new Scanner(System.in);
             while(sc.hasNext()){
                 Queue<Character> queue1 = new PriorityQueue<>();
