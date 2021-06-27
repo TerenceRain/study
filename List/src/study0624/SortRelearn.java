64cd3db59ce672e2ba223545daf24e9bced46e29
Terence <1403973494@qq.com>
2021-06-27 23:11:02 +0800

    更新代码

--- List/src/study0624/SortRelearn.java
+++ List/src/study0624/SortRelearn.java
@@ -43,10 +43,31 @@ public class SortRelearn {
             arr[cur + gap] = v;
         }
     }
+
+    public static int[] selectSort(int[] arr) {
+        int bound = 0;
+        for(; bound < arr.length; bound++){//这就是排序的趟数
+            int cur = bound + 1;
+            for(; cur < arr.length; cur++){//内层循环就是打擂台的过程，遍历无序区间
+                if (arr[cur] < arr[bound]) {//如果有元素比擂主小，就和擂主交换位置
+                    swap(arr, cur, bound);
+                }
+            }
+        }
+        return arr;
+
+    }
+    public static void swap(int[] arr, int x, int y) {
+        int tmp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = tmp;
+    }
     public static void main(String[] args) {
         int[] arr = {9, 5, 8, 3, 7, 2};
-        int[] out = insertSort(arr);
+//        int[] out = insertSort(arr);
 //        int[] out = shellSort(arr);
+        int[] out = selectSort(arr);
         System.out.println(Arrays.toString(out));
     }
+
 }
