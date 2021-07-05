1d3007ccff9dfd27b8cf66d9ddb0bb60a8e823d4
Terence <1403973494@qq.com>
2021-07-05 21:25:25 +0800

    更新代码

---
+++ List/src/study0624/Solution.java
@@ -0,0 +1,31 @@
+package study0624;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void reverseWords(String s) {
+        s = s.trim();
+//        StringBuilder sb = new StringBuilder(s);
+//        sb.reverse();
+        String[] str =s.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i] + "/");
+        }
+
+//        StringBuilder sb2 = new StringBuilder();
+//        for(int i = 0; i < str.length - 1; i++){
+//            StringBuilder sb1 = new StringBuilder(str[i]);
+//            sb1.reverse();
+//            sb2.append(sb1.toString());
+//            sb2.append(" ");
+//        }
+//        sb2.append(new StringBuilder(str[str.length - 1]).reverse());
+//        return sb2.toString();
+//
+    }
+
+    public static void main(String[] args) {
+        reverseWords("   he  ll o   !");
+    }
+
+}
